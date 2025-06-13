e9917151eeb98526fa1771571672922467828daf
ruud-ctrl <me@ruudjuffermans.nl>
2025-06-14 01:32:06 +0200

    new test

--- backend/index.js
+++ backend/index.js
@@ -6,15 +6,15 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(cors());
 
 app.use(express.json());
 
 app.get("/api/users", async (req, res) => {
-  const { rows } = await db.query("SELECT * FROM users");
-  res.json(rows);
+  // const { rows } = await db.query("SELECT * FROM users");
+  res.json({key : "val"});
 });
 
 app.listen(PORT, () => {
